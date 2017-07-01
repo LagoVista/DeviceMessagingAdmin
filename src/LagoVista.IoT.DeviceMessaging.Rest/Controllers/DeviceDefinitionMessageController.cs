590a0e77bf461f4eadbdc6d204aa815b10b67597
Kevin D. Wolf <kevinw@software-logistics.com>
2017-07-01 17:43:59 -0400

    messages now flowing through to UI!

--- src/LagoVista.IoT.DeviceMessaging.Rest/Controllers/DeviceDefinitionMessageController.cs
+++ src/LagoVista.IoT.DeviceMessaging.Rest/Controllers/DeviceDefinitionMessageController.cs
@@ -31,22 +31,22 @@ namespace LagoVista.IoT.DeviceMessaging.Rest.Controllers
 
 
         /// <summary>
-        /// Device Message Config - Add New
+        /// Device Message Type - Add New
         /// </summary>
         /// <param name="deviceMessageConfiguration"></param>
         /// <returns></returns>
-        [HttpPost("/api/devicemessageconfig")]
+        [HttpPost("/api/devicemessagetype")]
         public Task<InvokeResult> AddDeviceMessageConfigurationAsync([FromBody] DeviceMessageDefinition deviceMessageConfiguration)
         {
             return _deviceConfigManager.AddDeviceMessageDefinitionAsync(deviceMessageConfiguration, OrgEntityHeader, UserEntityHeader);
         }
 
         /// <summary>
-        /// Device Message Config - Update Config
+        /// Device Message Type - Update Config
         /// </summary>
         /// <param name="deviceMessageConfiguration"></param>
         /// <returns></returns>
-        [HttpPut("/api/devicemessageconfig")]
+        [HttpPut("/api/devicemessagetype")]
         public Task<InvokeResult> UpdateDeviceMessageConfigurationAsync([FromBody] DeviceMessageDefinition deviceMessageConfiguration)
         {
             SetUpdatedProperties(deviceMessageConfiguration);
@@ -54,11 +54,11 @@ namespace LagoVista.IoT.DeviceMessaging.Rest.Controllers
         }
 
         /// <summary>
-        /// Device Message Config - Get Configs for Org
+        /// Device Message Type - Get Configs for Org
         /// </summary>
         /// <param name="orgId">Organization Id</param>
         /// <returns></returns>
-        [HttpGet("/api/org/{orgid}/devicemessageconfigs")]
+        [HttpGet("/api/org/{orgid}/devicemessagetypes")]
         public async Task<ListResponse<DeviceMessageDefinitionSummary>> GetDeviceMessageConfigurationsForOrgAsync(String orgId)
         {
             var deviceMessageConfiguration = await _deviceConfigManager.GetDeviceMessageDefinitionsForOrgsAsync(orgId, UserEntityHeader);
@@ -66,11 +66,11 @@ namespace LagoVista.IoT.DeviceMessaging.Rest.Controllers
         }
 
         /// <summary>
-        /// Device Message Config - Get A Configuration
+        /// Device Message Type - Get A Configuration
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [HttpGet("/api/devicemessageconfig/{id}")]
+        [HttpGet("/api/devicemessagetype/{id}")]
         public async Task<DetailResponse<DeviceMessageDefinition>> GetDeviceMessageConfigurationAsync(String id)
         {
             var deviceMessageConfiguration = await _deviceConfigManager.GetDeviceMessageDefinitionAsync(id, OrgEntityHeader, UserEntityHeader);
@@ -78,41 +78,41 @@ namespace LagoVista.IoT.DeviceMessaging.Rest.Controllers
         }
 
         /// <summary>
-        /// Device Message Config - Check in Use
+        /// Device Message Type - Check in Use
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        [HttpGet("/api/devicemessageconfig/{id}/inuse")]
+        [HttpGet("/api/devicemessagetype/{id}/inuse")]
         public Task<DependentObjectCheckResult> InUseCheck(String id)
         {
             return _deviceConfigManager.CheckDeviceMessageInUseAsync(id, OrgEntityHeader, UserEntityHeader);
         }
 
         /// <summary>
-        /// Device Message Config - Key In Use
+        /// Device Message Type - Key In Use
         /// </summary>
         /// <returns></returns>
-        [HttpGet("/api/devicemessageconfig/{key}/keyinuse")]
+        [HttpGet("/api/devicemessagetype/{key}/keyinuse")]
         public Task<bool> DeviceConfigKeyInUse(String key)
         {
             return _deviceConfigManager.QueryDeviceMessageDefinitionKeyInUseAsync(key, CurrentOrgId);
         }
 
         /// <summary>
-        /// Device Message Config - Delete
+        /// Device Message Type - Delete
         /// </summary>
         /// <returns></returns>
-        [HttpDelete("/api/devicemessageconfig/{id}")]
+        [HttpDelete("/api/devicemessagetype/{id}")]
         public Task<InvokeResult> DeleteDeviceMessageConfigurationAsync(string id)
         {
             return _deviceConfigManager.DeleteDeviceMessageDefinitionAsync(id, OrgEntityHeader, UserEntityHeader);
         }
 
         /// <summary>
-        ///  Device Message Config - Create New
+        ///  Device Message Type - Create New
         /// </summary>
         /// <returns></returns>
-        [HttpGet("/api/devicemessageconfig/factory")]
+        [HttpGet("/api/devicemessagetype/factory")]
         public DetailResponse<DeviceMessageDefinition> CreateDeviceMessageConfigurartion()
         {
             var response = DetailResponse<DeviceMessageDefinition>.Create();
@@ -124,20 +124,20 @@ namespace LagoVista.IoT.DeviceMessaging.Rest.Controllers
         }
 
         /// <summary>
-        ///  Device Message Sample- Create New
+        ///  Device Message Type, Sample- Create New
         /// </summary>
         /// <returns></returns>
-        [HttpGet("/api/devicemessageconfig/samplemessage/factory")]
+        [HttpGet("/api/devicemessagetype/samplemessage/factory")]
         public DetailResponse<SampleMessage> CreateSampleMessage()
         {
             return DetailResponse<SampleMessage>.Create();
         }
 
         /// <summary>
-        ///  Device Message Faming Byte - Create New
+        ///  Device Message Type, Framing Byte - Create New
         /// </summary>
         /// <returns></returns>
-        [HttpGet("/api/devicemessageconfig/framingbyte/factory")]
+        [HttpGet("/api/devicemessagetype/framingbyte/factory")]
         public DetailResponse<MessageFramingBytes> CreateFramingByte()
         {
             return DetailResponse<MessageFramingBytes>.Create();
@@ -145,14 +145,13 @@ namespace LagoVista.IoT.DeviceMessaging.Rest.Controllers
 
 
         /// <summary>
-        ///  Device Message Config - Create New
+        ///  Device Message Type, Field - Create New
         /// </summary>
         /// <returns></returns>
-        [HttpGet("/api/devicemessagefield/factory")]
+        [HttpGet("/api/devicemessagetype/field/factory")]
         public DetailResponse<DeviceMessageDefinitionField> CreateDeviceMessageField()
         {
-            var response = DetailResponse<DeviceMessageDefinitionField>.Create();
-            return response;
+            return DetailResponse<DeviceMessageDefinitionField>.Create();
         }
     }
 }
